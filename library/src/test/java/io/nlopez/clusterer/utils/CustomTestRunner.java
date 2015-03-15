3587ed5f2282d1f03232c9aeb57700eaf8bb2614
Nacho L <nacho@nlopez.io>
2015-03-15 23:20:42 +0000

    Switched to Androids unit testing and ditched the robojava plugin

---
+++ library/src/test/java/io/nlopez/clusterer/utils/CustomTestRunner.java
@@ -0,0 +1,13 @@
+package io.nlopez.clusterer.utils;
+
+import org.junit.runners.model.InitializationError;
+import org.robolectric.RobolectricTestRunner;
+
+/**
+ * Created by mrm on 9/1/15.
+ */
+public class CustomTestRunner extends RobolectricTestRunner {
+    public CustomTestRunner(Class<?> testClass) throws InitializationError {
+        super(testClass);
+    }
+}
